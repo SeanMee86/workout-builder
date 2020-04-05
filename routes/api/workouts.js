30ea9259d7db0a36d0bc9756b5b4f3d28fc44e63
SeanMee86 <sean.mee86@yahoo.com>
2020-04-04 18:45:33 -0700

    code cleaning'

--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = module.exports = express.Router();
-
 const { connect } = require('mongoose');
-
 require('dotenv').config();
-
-const insertExercise = require('../../utils/dbInserts/exercises');
 const getUser = require('../../utils/dbRetrieve/user');
-
 const connectionString = process.env.CONNECTION_STRING;
 const connectionOptions = {useNewUrlParser: true, useUnifiedTopology: true};
 
