c4cb0070c614eaa1f43baaefb58e1334ea97ff0c
hss-smee <seanmee8186@gmail.com>
2020-04-11 18:46:02 -0700

    working on workouts container

--- client/src/containers/Workouts.js
+++ client/src/containers/Workouts.js
@@ -4,17 +4,33 @@ import {getWorkouts} from "../store/actions/workouts";
 
 class Workouts extends Component{
 
+    state = {
+        showWorkouts: true
+    };
+
     componentDidMount() {
         this.props.getWorkouts();
     }
 
     render() {
+        const workouts = this.props.workouts.map((workout, ind) => {
+            return (
+                <div onClick={() => this.setState({showWorkouts: false})} key={ind}>
+                    <h2>{workout.name}</h2>
+                    <p>{workout.workout.length} exercise{workout.workout.length !== 1 ? 's' : ''}</p>
+                </div>
+            )
+        });
         return(
             <div>
-                Hello
+                {this.state.showWorkouts ? workouts : null}
             </div>
         )
     }
 }
 
-export default connect(null, {getWorkouts})(Workouts);
+const mapStateToProps = state => ({
+    workouts: state.workouts.workouts
+});
+
+export default connect(mapStateToProps, {getWorkouts})(Workouts);
