1f3b30f56ca9b778a6c89ab3cb12cb3f936f050c
SeanMee86 <sean.mee86@yahoo.com>
2020-04-14 06:00:45 -0700

    finished hamburger menu

--- client/src/components/UI/Nav/Nav.js
+++ client/src/components/UI/Nav/Nav.js
@@ -2,25 +2,51 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 import classes from './Nav.module.scss'
 
-const Nav = () => (
-    <header>
-        <div className={classes.menuWrapper}>
-            <input title={'Menu Box'} type="checkbox" className={classes.toggler}/>
-            <div className={classes.hamburger}><div></div></div>
-            <nav className={classes.menu}>
-                <div>
-                    <div>
-                        <ul className={classes.list}>
-                            <li><NavLink activeClassName={classes.active} exact to={'/'}>Home</NavLink></li>
-                            <li><NavLink activeClassName={classes.active} to={'/workout-builder'}>Workout Builder</NavLink></li>
-                            <li><NavLink activeClassName={classes.active} to={'/workouts'}>Your Workouts</NavLink></li>
-                            <li><NavLink activeClassName={classes.active} to={'/exercises'}>Exercises</NavLink></li>
-                        </ul>
-                    </div>
+const Nav = () => {
+
+    const removeSideDrawer = () => {
+        document.getElementById('menu-toggle').checked = false;
+    };
+
+    return (
+        <header>
+            <div className={classes.MenuWrapper}>
+                <input id={'menu-toggle'} title={'Menu Box'} type="checkbox" className={classes.Toggler}/>
+                <div className={classes.Hamburger}>
+                    <div></div>
                 </div>
-            </nav>
-        </div>
-    </header>
-);
+                <nav className={classes.Menu}>
+                    <div>
+                        <div>
+                            <ul className={classes.List}>
+                                <li onClick={removeSideDrawer}>
+                                    <NavLink
+                                        activeClassName={classes.active}
+                                        exact
+                                        to={'/'}>Home</NavLink>
+                                </li>
+                                <li onClick={removeSideDrawer}>
+                                    <NavLink
+                                        activeClassName={classes.active}
+                                        to={'/workout-builder'}>Workout Builder</NavLink>
+                                </li>
+                                <li onClick={removeSideDrawer}>
+                                    <NavLink
+                                        activeClassName={classes.active}
+                                        to={'/workouts'}>Your Workouts</NavLink>
+                                </li>
+                                <li onClick={removeSideDrawer}>
+                                    <NavLink
+                                        activeClassName={classes.active}
+                                        to={'/exercises'}>Exercises</NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    );
+}
 
 export default Nav;
