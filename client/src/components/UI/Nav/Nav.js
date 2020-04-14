7a7de300962d32797a92ddace299ee7583405f1b
hss-smee <seanmee8186@gmail.com>
2020-04-14 05:00:40 -0700

    hamburge mobile

--- client/src/components/UI/Nav/Nav.js
+++ client/src/components/UI/Nav/Nav.js
@@ -3,12 +3,24 @@ import {NavLink} from "react-router-dom";
 import classes from './Nav.module.scss'
 
 const Nav = () => (
-    <ul className={classes.Nav}>
-        <li><NavLink activeClassName={classes.active} exact to={'/'}>Home</NavLink></li>
-        <li><NavLink activeClassName={classes.active} to={'/workout-builder'}>Workout Builder</NavLink></li>
-        <li><NavLink activeClassName={classes.active} to={'/workouts'}>Your Workouts</NavLink></li>
-        <li><NavLink activeClassName={classes.active} to={'/exercises'}>Exercises</NavLink></li>
-    </ul>
+    <header>
+        <div className={classes.menuWrapper}>
+            <input title={'Menu Box'} type="checkbox" className={classes.toggler}/>
+            <div className={classes.hamburger}><div></div></div>
+            <nav className={classes.menu}>
+                <div>
+                    <div>
+                        <ul className={classes.list}>
+                            <li><NavLink activeClassName={classes.active} exact to={'/'}>Home</NavLink></li>
+                            <li><NavLink activeClassName={classes.active} to={'/workout-builder'}>Workout Builder</NavLink></li>
+                            <li><NavLink activeClassName={classes.active} to={'/workouts'}>Your Workouts</NavLink></li>
+                            <li><NavLink activeClassName={classes.active} to={'/exercises'}>Exercises</NavLink></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
 );
 
 export default Nav;
