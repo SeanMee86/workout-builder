9fcfa9bdc42bcf50cf9d7664251e47272e79c1de
SeanMee86 <sean.mee86@yahoo.com>
2020-04-19 11:56:42 -0700

    updating header

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Link, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 
--- client/src/components/UI/Header/Header.js
+++ client/src/components/UI/Header/Header.js
@@ -3,9 +3,14 @@ import { Link } from "react-router-dom";
 
 import classes from './Header.module.scss';
 
+import siteLogo from '../../../shared/images/gym-1048852_1280.png';
+
 const Header = () => (
     <div className={classes.Heading}>
-        <Link to={'/'}><h1>Workout Builder</h1></Link>
+        <Link to={'/'}>
+            <img className={classes.HeaderLogo} src={siteLogo} alt=""/>
+            <h1>Workout Builder</h1>
+        </Link>
     </div>
 );
 
