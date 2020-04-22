84d265455ca765444e486cdefe2b73d64b207009
SeanMee86 <sean.mee86@yahoo.com>
2020-04-21 17:55:58 -0700

    updating user disconnect function

--- utils/sockets/index.js
+++ utils/sockets/index.js
@@ -1,11 +1,22 @@
 const chatHistory = [];
 
+let currentUsers = [];
+
+const userDisconnect = (io, socket, event) => {
+    return socket.on(event, () => {
+        const user = currentUsers.filter(user => user.userId === socket.id);
+        currentUsers = currentUsers.filter(user => user.userId !== socket.id);
+        io.emit('userDisconnected', `${user[0] ? user[0].userName : null} Has Left The Chat`);
+    });
+};
+
 module.exports = (io) => {
     io.on('connection', socket => {
         socket.on('getChatLogs', () => {
             socket.emit('receiveChatLogs', chatHistory);
         });
         socket.on('userConnected', userData => {
+            currentUsers.push({userName: userData.user, userId: socket.id});
             io.emit('userConnected', `${userData.user} Has Joined The Chat`);
         });
         socket.on('messageSent', (message) => {
@@ -15,8 +26,7 @@ module.exports = (io) => {
             }
             io.emit('messageToClients', message);
         });
-        socket.on('userDisconnected', userData => {
-            io.emit('userDisconnected', `${userData.user} Has Left The Chat`);
-        });
+        userDisconnect(io, socket, 'userDisconnected');
+        userDisconnect(io, socket, 'disconnect');
     });
 };
